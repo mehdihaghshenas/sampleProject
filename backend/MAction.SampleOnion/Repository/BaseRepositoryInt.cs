ae7844f7c4c80033615f290b10908156a47b688d
Mehdi Haghshenas <mhaghshenas@gmail.com>
2022-07-16 12:27:43 +0430

    Fix Build Problem

---
+++ backend/MAction.SampleOnion/Repository/BaseRepositoryInt.cs
@@ -0,0 +1,19 @@
+﻿
+using MAction.BaseClasses;
+using MAction.BaseMongoRepository;
+using MongoDB.Driver;
+
+namespace MAction.SampleOnion.Repository;
+
+public class BaseRepositoryInt<T> : MongoRepository<T, int>, IBaseRepositoryInt<T> where T : IBaseEntity
+{
+    public interface IBaseRepositoryInt<T> : IMongoRepository<T, int> where T : IBaseEntity
+    {
+    }
+
+    public BaseRepositoryInt(IMongoDependencyProvider databaseName, IMongoClient mongoClient, IBaseServiceDependencyProvider baseServiceDependencyProvider):
+    base(databaseName,mongoClient,baseServiceDependencyProvider)
+    {
+        
+    }
+}
--- backend/MAction.SampleOnion/Repository/BaseRepositoryObjectId.cs
+++ backend/MAction.SampleOnion/Repository/BaseRepositoryObjectId.cs
@@ -5,19 +5,6 @@ using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace MAction.SampleOnion.Repository;
-public class BaseRepositoryInt<T> : MongoRepository<T, int>, IBaseRepositoryInt<T> where T : IBaseEntity
-{
-    public BaseRepositoryInt(IMongoDependencyProvider databaseName, IMongoClient mongoClient, IBaseServiceDependencyProvider baseServiceDependencyProvider):
-    base(databaseName,mongoClient,baseServiceDependencyProvider)
-    {
-        
-    }
-}
-
-public interface IBaseRepositoryInt<T> : IMongoRepository<T, int> where T : IBaseEntity
-{
-}
-
 public class BaseRepositoryObjectId<T> : MongoRepository<T, ObjectId>, IBaseRepositoryObjectId<T> where T : IBaseEntity
 {
     public BaseRepositoryObjectId(IMongoDependencyProvider databaseName, IMongoClient mongoClient, IBaseServiceDependencyProvider baseServiceDependencyProvider):
